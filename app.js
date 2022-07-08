a2c6b8dbdca94c5c6714deeebbbe11c1c6413e6e
Ethan <elnair122@gmail.com>
2022-07-08 11:36:33 -0400

    done

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const { db, Page, User } = require('./models');
-const wikiRouter = require('./routes/wiki');
-const userRouter = require('./routes/users');
+const morgan = require("morgan");
+// const { db, Page, User } = require('./models');
+const wikiRouter = require("./routes/wiki");
+const userRouter = require("./routes/users");
+const path = require("path");
 
-app.use('/', morgan('dev'));
+app.use(morgan("dev")); //logging middleware
+app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
+app.use(express.urlencoded({ extended: false })); //parsing middleware for form input data
+app.use(express.json());
+app.use(require("method-override")("_method"));
 
-app.use(express.static(__dirname + "/public"));
+app.use("/users", userRouter);
+app.use("/wiki", wikiRouter);
 
+app.get("/", (req, res, next) => {
+  res.redirect("/wiki");
+});
 
-// app.use('/user', userRouter);
+module.exports = app;
 
-app.use('/wiki', wikiRouter);
+// db. authenticate is not required to have
+// db.authenticate()
+//   .then(() => {
+//     console.log('connected to the database');
+//   });
 
-app.get('/', (req, res, next) => {
-  res.redirect('/wiki')
-})
+// you can also make a seperate file
+// for what is below (e.g. server.js)
+// const PORT = 3000;
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  });
+// const init = async () => {
+//   try {
+//     // Reference: https://sequelize.org/master/manual/model-basics.html#model-synchronization
+//     // `.sync()` creates the table in the database if it doesn't exist (and does nothing if it already exists)
+//     // We can do this two ways:
+//     // 1. call `.sync()` on each individual Sequelize model (see commented out code below), or
+//     // await Page.sync();
+//     // await User.sync();
 
-  const PORT = 3000;
+//     // 2. call `.sync()` on the entire Sequelize instance (e.g. `db`) since our models are defined on it (i.e. `db.define(...)`)
+//     await db.sync();
 
-  const init = async () => {
-    await Page.sync();
-    await User.sync();
-    await db.sync({force: true});
-    // make sure that you have a PORT constant
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}!`);
-    });
-  }
-  
-  init();
+//     app.listen(PORT, () => {
+//       console.log(`Listening at http://localhost:${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Error starting server:', error)
+//   }
+// };
 
-
-
-
+// init();
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,44 +1,163 @@
-const express = require('express');
-const { rmSync } = require('fs');
-const { extendWith } = require('lodash');
+const express = require("express");
 const router = express.Router();
-const { Page } = require("../models");
-const { addPage } = require("../views");
+const { Page, User } = require("../models");
+const { main, addPage, editPage, wikiPage } = require("../views");
 
-router.get('/', (req, res, next) => {
-    res.send('Hi')
+// GET /wiki
+router.get("/", async (req, res, next) => {
+  try {
+    // const pages = await Page.findAll();
+    res.send('hi');
+    // res.send(main(pages));
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', async (req, res, next) => {
+// POST /wiki
+router.post("/", async (req, res, next) => {
+  try {
+    const [user, wasCreated] = await User.findOrCreate({
+      where: {
+        name: req.body.name,
+        email: req.body.email
+      }
+    });
 
-    // STUDENT ASSIGNMENT:
-    // add definitions for `title` and `content`
-  
-    try {
-      const page = await Page.create({
-        title: req.params.title,
-        content: req.params.content
-      });
-  
-      // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
-      res.redirect('/');
-    } catch (error) { next(error) }
-  });
+    const page = await Page.create(req.body);
 
-router.get('/add', (req, res, next) => {
-    res.send(addPage());
+    await page.setAuthor(user);
+
+    res.redirect("/wiki" + page.slug);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:slug', async (req, res, next) => {
+// POST /wiki/:slug
+router.put("/:slug", async (req, res, next) => {
+  try {
+    const [updatedRowCount, updatedPages] = await Page.update(req.body, {
+      where: {
+        slug: req.params.slug
+      },
+      returning: true
+    });
+
+    res.redirect("/wiki/" + updatedPages[0].slug);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /wiki/:slug
+router.delete("/:slug", async (req, res, next) => {
+  try {
+    await Page.destroy({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    res.redirect("/wiki");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /wiki/add
+router.get("/add", (req, res) => {
+  res.send(addPage());
+});
+
+// GET /wiki/:slug
+router.get("/:slug", async (req, res, next) => {
   try {
     const page = await Page.findOne({
       where: {
         slug: req.params.slug
       }
     });
-    res.json(page)
-  } catch (error) { next(error)}
-  res.send(`hit dynamic route at ${req.params.slug}`)
-})
+    if (page === null) {
+      res.sendStatus(404);
+    } else {
+      const author = await page.getAuthor();
+      res.send(wikiPage(page, author));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /wiki/:slug/edit
+router.get("/:slug/edit", async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+
+    if (page === null) {
+      res.sendStatus(404);
+    } else {
+      const author = await page.getAuthor();
+      res.send(editPage(page, author));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
+
+
+
+
+
+
+// Original attempt:
+// const express = require('express');
+// const { rmSync } = require('fs');
+// const { extendWith } = require('lodash');
+// const router = express.Router();
+// const { Page } = require("../models");
+// const { addPage } = require("../views");
+
+// router.get('/', (req, res, next) => {
+//     res.send('Hi')
+// });
+
+// router.post('/', async (req, res, next) => {
+
+//     // STUDENT ASSIGNMENT:
+//     // add definitions for `title` and `content`
+  
+//     try {
+//       const page = await Page.create({
+//         title: req.params.title,
+//         content: req.params.content
+//       });
+  
+//       // make sure we only redirect *after* our save is complete! Don't forget to `await` the previous step. `create` returns a Promise.
+//       res.redirect('/');
+//     } catch (error) { next(error) }
+//   });
+
+// router.get('/add', (req, res, next) => {
+//     res.send(addPage());
+// });
+
+// router.get('/:slug', async (req, res, next) => {
+//   try {
+//     const page = await Page.findOne({
+//       where: {
+//         slug: req.params.slug
+//       }
+//     });
+//     res.json(page)
+//   } catch (error) { next(error)}
+//   res.send(`hit dynamic route at ${req.params.slug}`)
+// })
+
+// module.exports = router;
